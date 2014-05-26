35d104eade69dddbd53731395b2255b94a4941ca
unknown <daniel@danielstern.ca>
2014-05-26 16:08:53 -0400

    release is fairly stable

--- controllers/StockMarket.js
+++ controllers/StockMarket.js
@@ -1,7 +1,6 @@
 angular.module("BillionaireGame")
     .controller("StockMarket", function($scope, $rootScope) {
 
-
         $scope.openBuyStockModal = function(stock) {
 
             var deal = {
@@ -52,6 +51,66 @@ angular.module("BillionaireGame")
             });
         }
 
+
+        $scope.openSellStockModal = function(holding) {
+
+            $scope.deal = holding;
+            holding.amountToSell = holding.length;
+
+            $('#stockSellModal').modal();
+            $('#stockSellModal').on('hidden.bs.modal', function() {
+                if (!$scope.session.confirming) $scope.unpause();
+            });
+
+
+            $scope.pause();
+        }
+
+
+        $scope.confirmSellStock = function(holding, count) {
+
+            $scope.pause();
+
+            for (var i = 0; i < count; i++) {
+               var stock = _.find($scope.session.player.stocks,function(stock){
+                    if (stock.originalNetCost == holding[0].originalNetCost
+                        && stock.boughtDate == holding[0].boughtDate
+                        && stock.link == holding[0].link) {
+                          return true;
+                    }
+               });
+
+               $scope.session.player.stocks = _.without($scope.session.player.stocks, stock);
+            }
+
+            $scope.session.player.cash += (count * holding[0].link.price);
+
+            $('#stockSellModal').modal('hide');
+            if (!$scope.session.confirming) $scope.unpause();
+         
+        }
+
+        $rootScope.getROR = function(stock) {
+
+            if (!stock) return;
+
+            var numMonths = $scope.session.world.month - stock.boughtDate;
+            var startingVal = stock.originalNetCost;
+            var currentVal = stock.link.price;
+
+            if (!numMonths) return;
+
+            var res = Calculon.rateOfReturn({
+                interestRate: null,
+                startingValue: startingVal,
+                finalValue: currentVal,
+                numMonths: numMonths
+            });
+
+            return res.value;
+        }
+
+
         $rootScope.consolidateStocks = function() {
 
             var consolidated = [];
@@ -75,7 +134,7 @@ angular.module("BillionaireGame")
 
         }
 
-        $scope.expandStocks = function(holding) {
+        $rootScope.expandStocks = function(holding) {
 
             var stocks = [];
             for (var i = 0; i < holding.count; i++) {
@@ -89,71 +148,6 @@ angular.module("BillionaireGame")
             }
 
             return stocks;
-        }
-
-        $scope.openSellStockModal = function(holding) {
-
-            $scope.deal = holding;
-            holding.amountToSell = holding.length;
-            //var scope = $scope;
-            //debugger;
-
-            $('#stockSellModal').modal();
-            $('#stockSellModal').on('hidden.bs.modal', function() {
-                if (!$scope.session.confirming) $scope.unpause();
-            });
-
-
-            $scope.pause();
-        }
-
-
-        $scope.confirmSellStock = function(holding, count) {
-
-            $scope.pause();
-
-            console.log("Selling stock",holding);
-           // debugger;
-
-            holding.settled = false;
-
-            for (var i = 0; i < count; i++) {
-               var stock = _.find($scope.session.player.stocks,function(stock){
-                    if (stock.originalNetCost == holding[0].originalNetCost
-                        && stock.boughtDate == holding[0].boughtDate
-                        && stock.link == holding[0].link) {
-                          return true;
-                    }
-               });
-
-               $scope.session.player.stocks = _.without($scope.session.player.stocks, stock);
-            }
-
-            $scope.session.player.cash += (count * holding[0].link.price);
-
-            $('#stockSellModal').modal('hide');
-            if (!$scope.session.confirming) $scope.unpause();
-         
-        }
-
-        $scope.getROR = function(stock) {
-
-            if (!stock) return;
-
-            var numMonths = $scope.session.world.month - stock.boughtDate;
-            var startingVal = stock.originalNetCost;
-            var currentVal = stock.link.price;
-
-            if (!numMonths) return;
-
-            var res = Calculon.rateOfReturn({
-                interestRate: null,
-                startingValue: startingVal,
-                finalValue: currentVal,
-                numMonths: numMonths
-            });
-
-            return res.value;
         }
 
     })
