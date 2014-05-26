d674a33ba080ef7033d59aac4961a3966b12b2d2
unknown <daniel@danielstern.ca>
2014-05-25 21:07:37 -0400

    stock market works

---
+++ Billionaire/billionaireStocks.js
@@ -0,0 +1,18 @@
+angular.module("BillionaireGame")
+    .service("billionaireStocks", function() {
+        var stocks = [{
+            name: "Evil Corporation",
+            symbol: "EVL",
+            category: "finance",
+            growthRate: 1.12,
+            bookValue: "40",
+        }, {
+            name: "Modulus Inc",
+            symbol: "MOD",
+            category: "technology",
+            growthRate: 1.35,
+            bookValue: "12"
+        }]
+
+        return stocks;
+    })
--- script.js
+++ script.js
@@ -1,54 +1,74 @@
+angular.module("BillionaireGame", [])
+    .controller("BillionaireGame", function($scope, billionaireStocks, $interval) {
 
-var session = undefined;
-var timer = undefined;
+        var session = undefined;
+        var timer = undefined;
 
-var defaultStats = {
-    player: {
-        cash: 100,
-        stocks: [{
-            symbol: "lol",
-            purchased: "00000",
-            cost: "100"
-        }],
-        realEstate: [],
-        businesses: []
-    },
-    world: {
-        month: 1,
-        dollarValue: 1,
-    },
-    stats: {
-        rateOfReturn: undefined
-    },
-    game: {
-    	paused: false,
-    	difficulty: 1,
-    	duration: 500,
-    	inflation: 1.02
-    }
-}
+        var defaultStats = {
+            player: {
+                cash: 100,
+                stocks: [{
+                    symbol: "lol",
+                    purchased: "00000",
+                    cost: "100"
+                }],
+                karma: 0,
+                realEstate: [],
+                businesses: []
+            },
+            world: {
+                month: 1,
+                dollarValue: 1,
+            },
+            stats: {
+                rateOfReturn: undefined
+            },
+            market: {
+            	stocks: undefined
+            },
+            game: {
+                paused: false,
+                difficulty: 1,
+                duration: 500,
+                inflation: 1.02
+            }
+        }
 
-function gameTick() {
-    //	console.log("TICK");
-    session.world.month += 1;
-    session.world.dollarValue /= 1 + ((session.game.inflation - 1)  /12);
-    console.clear();
-    console.log(session.world);
+        function gameTick() {
+            session.world.month += 1;
+            session.world.dollarValue /= 1 + ((session.game.inflation - 1) / 12);
 
-    if (session.world.month > session.game.duration) {
-    	gameOver();
-    }
-}
+            _.each(session.market.stocks,function(stock){
+            	stock.bookValue = stock.bookValue * 1 + ((stock.growthRate - 1) / 12)
+            })
 
-function gameOver() {
-	console.log("Game over man!");
-	clearInterval(timer);
-}
 
-newGame();
+            console.clear();
+            console.log("STOCKS?" ,session.market.stocks[0]);
+            console.log(session.world);
 
-function newGame() {
-	console.log("%c If I had a billion dollars", "color:#f0f");
-	session = _.clone(defaultStats);
-	timer = setInterval(gameTick, 100);
-}
+            if (session.world.month > session.game.duration) {
+                gameOver();
+            }
+        }
+
+        function gameOver() {
+            console.log("Game over man!");
+            clearInterval(timer);
+        }
+
+        newGame();
+
+        function newGame() {
+            console.log("%c If I had a billion dollars", "color:#f0f");
+            var stocks = _.clone(billionaireStocks);
+
+            session = _.clone(defaultStats);
+            session.market.stocks = stocks;
+
+            timer = $interval(gameTick, 100);
+
+            $scope.session = session;
+        }
+
+    })
