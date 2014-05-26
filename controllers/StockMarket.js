8708d2bb461bda1295db0588c953c2554a373153
unknown <daniel@danielstern.ca>
2014-05-26 16:04:32 -0400

    everything works!

--- controllers/StockMarket.js
+++ controllers/StockMarket.js
@@ -52,26 +52,11 @@ angular.module("BillionaireGame")
             });
         }
 
-        $scope.openSellStockModal = function(holding) {
-
-            $scope.deal = holding;
-
-            $('#stockSellModal').modal();
-            $('#stockSellModal').on('hidden.bs.modal', function() {
-                if (!$scope.session.confirming) $scope.unpause();
-            });
-
-
-            $scope.pause();
-        }
-
         $rootScope.consolidateStocks = function() {
 
             var consolidated = [];
 
             var stocks = _.clone($scope.session.player.stocks);
-
-            console.log("Consolidating stocks");
 
             var holdings = _.groupBy(stocks,function(stock){
                 return JSON.stringify({
@@ -85,8 +70,6 @@ angular.module("BillionaireGame")
                 holding.link = holding[0].link;
                 consolidated.push(holding);
             })
-
-            console.log("Holdings?",holdings);
 
             return consolidated;
 
@@ -108,26 +91,49 @@ angular.module("BillionaireGame")
             return stocks;
         }
 
+        $scope.openSellStockModal = function(holding) {
+
+            $scope.deal = holding;
+            holding.amountToSell = holding.length;
+            //var scope = $scope;
+            //debugger;
+
+            $('#stockSellModal').modal();
+            $('#stockSellModal').on('hidden.bs.modal', function() {
+                if (!$scope.session.confirming) $scope.unpause();
+            });
+
+
+            $scope.pause();
+        }
+
+
         $scope.confirmSellStock = function(holding, count) {
 
             $scope.pause();
-            $scope.session.confirming = true;
-            $('#stockBuyModal').modal('hide');
 
-            deal.count -= count;
+            console.log("Selling stock",holding);
+           // debugger;
 
-            var transaction = {
-                amountSold: count,
-                stock: deal.link,
-                price: deal.soldAt,
+            holding.settled = false;
+
+            for (var i = 0; i < count; i++) {
+               var stock = _.find($scope.session.player.stocks,function(stock){
+                    if (stock.originalNetCost == holding[0].originalNetCost
+                        && stock.boughtDate == holding[0].boughtDate
+                        && stock.link == holding[0].link) {
+                          return true;
+                    }
+               });
+
+               $scope.session.player.stocks = _.without($scope.session.player.stocks, stock);
             }
 
-            $scope.session.player.cash += (deal.count);
-            $('#confirmStockBuyModal').modal();
-            $('#confirmStockBuyModal').on('hidden.bs.modal', function() {
-                $scope.unpause();
-                $scope.session.confirming = false;
-            });
+            $scope.session.player.cash += (count * holding[0].link.price);
+
+            $('#stockSellModal').modal('hide');
+            if (!$scope.session.confirming) $scope.unpause();
+         
         }
 
         $scope.getROR = function(stock) {
