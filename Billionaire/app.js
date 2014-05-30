c2a3fd1bdebb9b45321dcdf746b77a78c2feb67e
unknown <daniel@danielstern.ca>
2014-05-29 22:40:51 -0400

    routz works

--- Billionaire/app.js
+++ Billionaire/app.js
@@ -19,8 +19,22 @@ angular.module("BillionaireGame", [
             templateUrl: 'Billionaire/modules/BillionaireActions/billionaireActions.html',
             controller: 'BillionaireActionsController'
         }).
+        when('/jobs', {
+            templateUrl: 'Billionaire/modules/BillionaireJobs/billionaireJobs.html',
+            controller: 'BillionaireJobsController'
+        }).
+        when('/bank', {
+            templateUrl: 'Billionaire/modules/BillionaireLoans/billionaireLoans.html',
+            controller: 'BillionaireLoansController'
+        }).
+        when('/character', {
+            templateUrl: 'Billionaire/modules/BillionaireCharacter/billionaireCharacter.html',
+        }).
+        when('/scenario', {
+            templateUrl: 'Billionaire/modules/BillionaireScenario/billionaireScenario.html',
+        }).
         otherwise({
-            redirectTo: '/player'
+            redirectTo: '/character'
         });
     }
 ]);
