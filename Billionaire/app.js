6e10aa617bffa02b3836175415d66f50d79144f2
unknown <daniel@danielstern.ca>
2014-05-29 22:22:35 -0400

    rudimentary routing works! wow!

--- Billionaire/app.js
+++ Billionaire/app.js
@@ -12,9 +12,9 @@ angular.module("BillionaireGame",
 .config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-      when('/phones', {
-        templateUrl: 'partials/phone-list.html',
-        controller: 'PhoneListCtrl'
+      when('/stocks', {
+        templateUrl: 'Billionaire/modules/BillionaireStocks/billionaireStocks.html',
+        //controller: 'PhoneListCtrl'
       }).
       when('/phones/:phoneId', {
         templateUrl: 'partials/phone-detail.html',
